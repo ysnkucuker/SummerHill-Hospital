364d41bd2584d640987cf02c7fb2033b54dd1c2a
Yasin Kucuker <yasinkucuker@windowslive.com>
2023-11-07 11:21:32 +0300

    First

---
+++ deployment/native/styles.js
@@ -0,0 +1,12 @@
+import * as style0 from "C:/Users/yasin/Desktop/Yeni klasör/themesource/administration/native/main";
+import * as style1 from "C:/Users/yasin/Desktop/Yeni klasör/themesource/communitycommons/native/main";
+import * as style2 from "C:/Users/yasin/Desktop/Yeni klasör/themesource/nanoflowcommons/native/main";
+import * as style3 from "C:/Users/yasin/Desktop/Yeni klasör/themesource/webactions/native/main";
+import * as style4 from "C:/Users/yasin/Desktop/Yeni klasör/themesource/atlas_core/native/main";
+import * as style5 from "C:/Users/yasin/Desktop/Yeni klasör/themesource/feedbackmodule/native/main";
+import * as style6 from "C:/Users/yasin/Desktop/Yeni klasör/themesource/usermanager/native/main";
+import * as style7 from "C:/Users/yasin/Desktop/Yeni klasör/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6, style7]);
---
+++ deployment/web/widgets/widgets.js
@@ -0,0 +1,1 @@
+define(["CSVasTable/widget/CSVasTable"], {});
